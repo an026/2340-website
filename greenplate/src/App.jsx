8b7292a5b2f46666cb2908ff9c58afef2f0af674
an026 <allisonvu9@gmail.com>
2024-04-19 22:25:53 -0400

    Init.

---
+++ greenplate/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Introducing GreenPlate</h1>
+      <div className='subheading'>
+        <p>GreenPlate is a sustainable food management system designed to streamline
+          food consumption and shopping practices.
+          By tracking daily calorie intake, managing ingredient inventories, and facilitating
+          efficient grocery shopping, GreenPlate not only enhances dietary habits but
+          also ensures that food waste is reduced and resource use is optimized.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
